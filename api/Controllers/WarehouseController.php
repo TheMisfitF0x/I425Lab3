5961acf99a7a9041c8e45b4594e32c12bb7bf065
Aero48 <56200404+Aero48@users.noreply.github.com>
2023-11-13 14:14:51 -0500

    added warehouse endpoints

--- api/Controllers/WarehouseController.php
+++ api/Controllers/WarehouseController.php
@@ -8,5 +8,83 @@ use Warehouse\Models\Warehouse;
 
 class WarehouseController
 {
+    public function index(Request $request, Response $response, array $args){
+        $results = Warehouse::getWarehouses($request);
+        $code = array_key_exists("status", $results) ? 500 : 200;
 
+        return $response->withJson($results, $code, JSON_PRETTY_PRINT);
+    }
+
+    public function view(Request $request, Response $response, array $args)
+    {
+        $id = $args['id'];
+        $results = Warehouse:: getWarehouseById($id);
+        $code = array_key_exists("status", $results) ? 500 : 200;
+
+        return $response->withJson($results, $code, JSON_PRETTY_PRINT);
+    }
+
+    public function viewOrders(Request $request, Response $response, array $args){
+        $id = $args['id'];
+        $results = Warehouse::getOrdersByWarehouse($id);
+        $code = array_key_exists('status', $results) ? 500 : 200;
+        return $response->withJson($results, $code, JSON_PRETTY_PRINT);
+    }
+
+    public function viewProducts(Request $request, Response $response, array $args){
+        $id = $args['id'];
+        $results = Warehouse::getProductsByWarehouse($id);
+        $code = array_key_exists('status', $results) ? 500 : 200;
+        return $response->withJson($results, $code, JSON_PRETTY_PRINT);
+    }
+
+    public function create(Request $request, Response $response, array $args)
+    {
+        $warehouse = Warehouse::createWarehouse($request);
+        if ($warehouse->id) {
+            $results = [
+                'status' => 'Employee created',
+                'employee_uri' => '/employees/' . $warehouse->id,
+                'data' => $warehouse
+            ];
+            $code = 201;
+        } else {
+            $code = 500;
+        }
+
+        return $response->withJson($results, $code, JSON_PRETTY_PRINT);
+    }
+
+    public function update(Request $request, Response $response, array $args)
+    {
+        // Insert a new student
+        $warehosue = Warehouse::updateWarehouse($request);
+        if ($warehosue->id) {
+            $results = [
+                'status' => 'Employee updated',
+                'message_uri' => '/employee/' . $warehosue->id,
+                'data' => $warehosue
+            ];
+            $code = 200;
+        } else {
+            $code = 500;
+        }
+
+        return $response->withJson($results, $code, JSON_PRETTY_PRINT);
+    }
+
+    public function delete(Request $request, Response $response, array $args)
+    {
+        $id = $request->getAttribute('id');
+        Warehouse::deleteWarehouse($request);
+        if (Warehouse::find($id)->exists) {
+            return $response->withStatus(500);
+
+        } else {
+            $results = [
+                'status' => "Warehouse '/warehouses/$id' has been deleted."
+            ];
+            return $response->withJson($results, 200, JSON_PRETTY_PRINT);
+        }
+    }
 }
--- api/Models/Warehouse.php
+++ api/Models/Warehouse.php
@@ -5,5 +5,84 @@ namespace Warehouse\Models;
 use \Illuminate\Database\Eloquent\Model;
 class Warehouse extends Model
 {
+    // The table associated with this model
+    protected $table = 'warehouse';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
 
+    //one to many relationship
+    public function orders(){
+        return $this->hasMany(Order::class, 'Warehouse_Id');
+    }
+
+    //one to many relationship
+    public function products(){
+        return $this->hasMany(Product::class, 'Warehouse_Id');
+    }
+
+    public static function getWarehouses()
+    {
+        $warehouses = self::all();
+        return $warehouses;
+    }
+
+    public static function getWarehouseById($id)
+    {
+        $warehouse = self::findOrFail($id);
+        return $warehouse;
+    }
+
+    public static function getOrdersByWarehouse($id)
+    {
+        $orders = self::findOrFail($id)->orders;
+        return $orders;
+    }
+
+    public static function getProductsByWarehouse($id)
+    {
+        $products = self::findOrFail($id)->orders;
+        return $products;
+    }
+
+    public static function createWarehouse($request)
+    {
+        // Retrieve parameters from request body
+        $params = $request->getParsedBody();
+
+        // Create a new User instance
+        $warehouse = new Warehouse();
+
+        // Set the user's attributes
+        foreach ($params as $field => $value) {
+            $warehouse->$field = $value;
+        }
+
+        // Insert the user into the database
+        $warehouse->save();
+        return $warehouse;
+    }
+
+    public static function updateWarehouse($request)
+    {
+        // Retrieve parameters from request body
+        $params = $request->getParsedBody();
+
+        //Retrieve the user's id from url and then the user from the database
+        $id = $request->getAttribute('id');
+        $warehouse = self::findOrFail($id);
+
+        foreach ($params as $field => $value) {
+            $warehouse->$field = $value;
+        }
+
+        // Update the professor
+        $warehouse->save();
+        return $warehouse;
+    }
+
+    public static function deleteWarehouse($id)
+    {
+        $warehouse = self::findOrFail($id);
+        return ($warehouse->delete());
+    }
 }

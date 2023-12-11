c49f5b8294fe693dbb399484fe14719f9da4a922
Aero48 <56200404+Aero48@users.noreply.github.com>
2023-12-11 14:26:11 -0500

    more improvements to warehouses page

--- public/js/warehouse.js
+++ public/js/warehouse.js
@@ -19,6 +19,7 @@ function showWarehouses() {
 function displayWarehouses(warehouses) {
     let _html;
     _html = `<div class='content-row content-row-header'>
+        <div class='warehouse-id'>ID</div>
         <div class='warehouse-location'>Location</div>
         <div class='warehouse-leasenum'>Lease Number</div>
         <div class='warehouse-sqft'>Square Footage</div>
@@ -28,15 +29,16 @@ function displayWarehouses(warehouses) {
         let warehouse = warehouses[x];
         let cssClass = (x % 2 == 0) ? 'content-row' : 'content-row content-row-odd';
         _html += `<div id='content-row-${warehouse.id}' class='${cssClass}'>
-            <div class='warehouse-location'>
+            <div class='warehouse-id'>
                 <span class='list-key' data-warehouse='${warehouse.id}' 
-                     onclick=showUserPostsPreview('${warehouse.id}') 
-                     title='Get orders made for the warehouse'>${warehouse.location}
+                     onclick=showWarehouseOrdersPreview('${warehouse.id}') 
+                     title='Get orders made for the warehouse'>${warehouse.id}
                 </span>
             </div>
+            <div class='warehouse-location'>${warehouse.location}</div>
             <div class='warehouse-leasenum'>${warehouse.lease_num}</div>
             <div class='warehouse-sqft'>${warehouse.sqft}</div>
-            <div class='warehouse-monthlycost'>${warehouse.monthly_cost}</div>            
+            <div class='warehouse-monthlycost'>$${warehouse.monthly_cost}</div>            
             </div>`;
     }
     //Finally, update the page
